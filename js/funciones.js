4f475e77b40db2c72c3cc9287c7b0d9a1082f899
elvys <elvys.pavel@gmail.com>
2020-07-04 20:17:20 -0500

    final

---
+++ js/funciones.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function(){
+	var enlace = jQuery(".hambur").attr("href");
+
+	jQuery(".hambur").click(function (e) {
+		e.preventDefault();
+		abrirMenu();
+	});
+	jQuery("header .container nav a").click(function() {
+		var enlace = jQuery(this).attr("href");
+		abrirMenu();
+		jQuery("html,body").animate({
+			"scrollTop" : jQuery(enlace).position().top
+		},500)
+	})
+	// jQuery(".hambur").addClass("open");
+	// jQuery(".hambur").removeClass("hambur");
+
+	// if (jQuery(".hambur").hasClass("open") == true) {
+	// 	expression
+	// }else {
+		
+	// }
+});
+
+
+function abrirMenu() {
+	jQuery("header").toggleClass("open");
+		jQuery("header .container .hambur i").toggleClass("fa-times");
+		jQuery("header .container nav").toggleClass("open");
+}
